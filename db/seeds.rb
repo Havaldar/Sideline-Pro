6d2e243e722bd7d5775c40540bf2863769efc401
Havaldar <abhinavhavaldar@hotmail.com>
2015-06-20 15:51:45 +0200

    Initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Team.create name: "Baby Blue"
+Team.create name: "Barcalona"
+Team.create name: "Real Madrid"
+Team.create name: "Bayern Munich"
+Team.create name: "Sevilla"
+Team.create name: "Bashiktas"
+Team.create name: "Galataserai"
