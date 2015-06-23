da3ef68e770e1fd286fec89912a5692de9ccdffc
meriah_oneil <meriahoneil@gmail.com>
2015-06-23 16:38:21 +0200

    editted user page a bit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,13 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+		@team = Team.new
+		@profile_name = User.name.upcase
 	end
 
 	def index
 		@users = User.all
 	end
+
+
 end
