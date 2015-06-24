878f6c106105ecfa7dfd92fe4f65f2b2de2338b2
meriah_oneil <meriahoneil@gmail.com>
2015-06-24 20:26:28 +0200

    team index and other stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Team.destroy_all
 
-Team.create name: "Baby Blue"
+Team.create name: "Baby Blue" 
 Team.create name: "Barcalona"
 Team.create name: "Real Madrid"
 Team.create name: "Bayern Munich"
 Team.create name: "Sevilla"
 Team.create name: "Bashiktas"
 Team.create name: "Galataserai"
+Team.create name: "Henrietta Hurricanes"
+Team.create name: "Red Wings"
+Team.create name: "Rochester Rhinos"
+Team.create name: "The Falcons"
+Team.create name: "Superman"
+Team.create name: "Fruit Loops"
+Team.create name: "Meow"
+
