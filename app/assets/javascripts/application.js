a5a874f5a4d33c603ec78e31c42d58aa22940fb2
meriah_oneil <meriahoneil@gmail.com>
2015-06-24 20:58:42 +0200

    repositioned teamshow page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,19 +29,3 @@ $(document).ready(function(){
     });
 });
 
-$(document).ready(function () {  
-  var top = $('#navbar').offset().top - parseFloat($('#navbar').css('marginTop').replace(/auto/, 100));
-  $(window).scroll(function (event) {
-    // what the y position of the scroll is
-    var y = $(this).scrollTop();
-
-    // whether that's below the form
-    if (y >= top) {
-      // if so, ad the fixed class
-      $('#navbar').addClass('fixed');
-    } else {
-      // otherwise remove it
-      $('#navbar').removeClass('fixed');
-    }
-  });
-});
